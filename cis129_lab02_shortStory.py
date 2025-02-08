ffbe059dde308a8a1706028efeebcb8bdea62338
ifoxmtz <ifox1@mail.pima.edu>
2025-02-07 20:34:48 -0700

    Update cis129_lab02_shortStory.py
    Created a short story about a puppy and replaced the hello, world statement.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Isabella Fox
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print("Once upon a time, in a world far away, lived a little puppy.")
+print("The puppy loved to roam outside amongst the foliage and fauna.")
+print("One day, the puppy stumbled upon a steaming hot pile of tacos.")
+print("She loved the smell of spiced meat and aromatic salsa and proceeded to gobble it up.")
+print("Content with a full belly, the puppy found a spot of sunlight to snooze the rest of the day away.")
