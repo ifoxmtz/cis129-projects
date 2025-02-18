6adcdd406a7ee2a11c33a2af0628f6cf8a66cf8e
ifoxmtz <ifox1@mail.pima.edu>
2025-02-18 12:34:53 -0700

    Update cis129_lab03_coffeeShop.py
    Added work from PyCharm into Github to begin revisions

--- cis129_lab03_coffeeShop.py
+++ cis129_lab03_coffeeShop.py
@@ -1,1 +1,24 @@
+#Declare my constants (name of item, number of items purchased, price of items)
+COFFEE_PRICE = 5
+MUFFIN_PRICE = 4
+TAX = int(0.06)
 
+#Display café name
+print('***************************************')
+print('My Coffee and Muffin Shop')
+
+#Create input variables for customer
+coffee = input("Number of coffees bought?")
+muffin = input("Number of muffins bought?")
+
+#Processing: Calculate the total cost
+coffee_total = coffee * COFFEE_PRICE
+muffin_total = muffin * MUFFIN_PRICE
+total_tax = TAX * (coffee_total + muffin_total)
+total_cost = total_tax + coffee_total + muffin_total
+
+#Output: Display the receipt to the customer
+    #display name of item, number of items bought, price, and total cost
+print('***************************************\n***************************************')
+print('My Coffee and Muffin Shop Receipt')
+print(coffee "Coffee at $" COFFEE_PRICE "each: $" coffee_total)
